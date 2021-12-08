094f9e99d20ad1d994dbeed58e11cc2f5d06bf72
Agnieszka Malaszkiewicz <agnieszka@fractalsoft.org>
2021-12-08 16:13:49 +0100

    Add Patterns module to Rule and Ruleset class

--- lib/patterns/strong_ruleset.rb
+++ lib/patterns/strong_ruleset.rb
@@ -1,19 +1,21 @@
 # StrongRuleset is not satisfied and not forceable if any of rules is not applicable
 
-class StrongRuleset < Ruleset
-  def satisfied?(force: false)
-    rules.all? do |rule|
-      (rule.applicable? && rule.satisfied?) || (force && rule.forceable?)
+module Patterns
+  class StrongRuleset < Ruleset
+    def satisfied?(force: false)
+      rules.all? do |rule|
+        (rule.applicable? && rule.satisfied?) || (force && rule.forceable?)
+      end
     end
-  end
 
-  def not_applicable?
-    rules.any?(&:not_applicable?)
-  end
+    def not_applicable?
+      rules.any?(&:not_applicable?)
+    end
 
-  def forceable?
-    rules.all? do |rule|
-      (rule.applicable? && rule.forceable?) || rule.satisfied?
+    def forceable?
+      rules.all? do |rule|
+        (rule.applicable? && rule.forceable?) || rule.satisfied?
+      end
     end
   end
 end
